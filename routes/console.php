d917e07fd7342eb7112866328528329daa5fa377
alooze <a.looze@gmail.com>
2024-10-08 11:01:49 +0300

    tmp

--- routes/console.php
+++ routes/console.php
@@ -5,9 +5,13 @@ use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schedule;
 use App\Domain\Orders\Commands\GrabMessages;
+use App\Domain\Orders\Commands\ParseMessages;
+use App\Domain\Orders\Commands\SendPosts;
 
 // Artisan::command('inspire', function () {
 //     $this->comment(Inspiring::quote());
 // })->purpose('Display an inspiring quote')->hourly();
 
 Schedule::command(GrabMessages::class)->everyMinute();
+Schedule::command(ParseMessages::class)->everyMinute();
+Schedule::command(SendPosts::class)->everyMinute();
